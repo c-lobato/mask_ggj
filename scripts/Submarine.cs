5f7d0bb7bea63c759cfe51168804a02c58809078
Falupe <l.maloni.c@gmail.com>
2026-01-30 19:11:28 -0300

    Water Level com tile map + ciração dos projeteis

--- scripts/Submarine.cs
+++ scripts/Submarine.cs
@@ -11,10 +11,18 @@ public partial class Submarine : RigidBody2D
 	[Export]
 	private float walkLimit;
 	private int walkedDistance;
+	[Export]
+	public PackedScene ProjectileScene { get; set; }
+	private Timer _shotTimer;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		animatedSprite.Play("default");
+		_shotTimer = GetNode<Timer>("Timer");
+		_shotTimer.Timeout += OnShotTimerTimeout;
+
+		_shotTimer.WaitTime = GD.RandRange(1.0, 3.0);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,6 +39,27 @@ public partial class Submarine : RigidBody2D
 			animatedSprite.FlipH = !animatedSprite.FlipH;
 			speed *= -1;
 		}
-		
+	}
+
+	public void OnShotTimerTimeout()
+	{
+		Shoot();
+	}
+
+	private void Shoot()
+	{
+		if (ProjectileScene == null) return;
+
+		var projetil = (Node2D)ProjectileScene.Instantiate();
+		projetil.GlobalPosition = GlobalPosition;
+		GetParent().AddChild(projetil);
+		GD.Print("Disparei");
+
+		// Implementar a direção do projetil
+		if (projetil is Projectile p) 
+        {
+            // Se o sprite está invertido, a direção é para a esquerda (-1), senão direita (1)
+            p.XDirection = animatedSprite.FlipH ? -1 : 1;
+        }
 	}
 }
